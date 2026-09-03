1843c07977a23a3db563c5e2c714716dd89d5cc9
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2026-09-03 11:00:49 +0200

    fin de seance 1

---
+++ embedded/ROBOT_Morlet_Lecat.X/ChipConfig.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ChipConfig.h
+ * Author: E306-PC4
+ *
+ * Created on September 3, 2026, 10:43 AM
+ */
+
+#ifndef CHIPCONFIG_H
+#define	CHIPCONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CHIPCONFIG_H */
+
---
+++ embedded/ROBOT_Morlet_Lecat.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: E306-PC4
+ *
+ * Created on September 3, 2026, 10:35 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
