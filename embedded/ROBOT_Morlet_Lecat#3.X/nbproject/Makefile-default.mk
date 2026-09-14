bd9208ccd0cb43e5c3517422a3a3d913096676aa
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2026-09-14 16:28:25 +0200

    projet version 3

---
+++ embedded/ROBOT_Morlet_Lecat#3.X/nbproject/Makefile-default.mk
@@ -0,0 +1,237 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1324871677/ADC.o ${OBJECTDIR}/_ext/1324871677/ChipConfig.o ${OBJECTDIR}/_ext/1324871677/IO.o ${OBJECTDIR}/_ext/1324871677/PWM.o ${OBJECTDIR}/_ext/1324871677/Robot.o ${OBJECTDIR}/_ext/1324871677/Timer.o ${OBJECTDIR}/_ext/1324871677/Toolbox.o ${OBJECTDIR}/_ext/1324871677/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1324871677/ADC.o.d ${OBJECTDIR}/_ext/1324871677/ChipConfig.o.d ${OBJECTDIR}/_ext/1324871677/IO.o.d ${OBJECTDIR}/_ext/1324871677/PWM.o.d ${OBJECTDIR}/_ext/1324871677/Robot.o.d ${OBJECTDIR}/_ext/1324871677/Timer.o.d ${OBJECTDIR}/_ext/1324871677/Toolbox.o.d ${OBJECTDIR}/_ext/1324871677/main.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/_ext/1324871677/ADC.o ${OBJECTDIR}/_ext/1324871677/ChipConfig.o ${OBJECTDIR}/_ext/1324871677/IO.o ${OBJECTDIR}/_ext/1324871677/PWM.o ${OBJECTDIR}/_ext/1324871677/Robot.o ${OBJECTDIR}/_ext/1324871677/Timer.o ${OBJECTDIR}/_ext/1324871677/Toolbox.o ${OBJECTDIR}/_ext/1324871677/main.o
+
+# Source Files
+SOURCEFILES=../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=33EP512MU814
+MP_LINKER_FILE_OPTION=,--script=p33EP512MU814.gld
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/1324871677/ADC.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c  .generated_files/flags/default/ff4c797132763ddf20c6781ae94aa6e4ae08aa6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ADC.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ADC.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c  -o ${OBJECTDIR}/_ext/1324871677/ADC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/ADC.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/ChipConfig.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c  .generated_files/flags/default/dff46fb612d853c75904119ec3bec6f18a0f1c9b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ChipConfig.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ChipConfig.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c  -o ${OBJECTDIR}/_ext/1324871677/ChipConfig.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/ChipConfig.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/IO.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c  .generated_files/flags/default/a3505e2468a05e14813ab1c7b8dc97332aff4212 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/IO.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/IO.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c  -o ${OBJECTDIR}/_ext/1324871677/IO.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/IO.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/PWM.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c  .generated_files/flags/default/973c924951cfc17d8ce75e15b2d2b36f83e1d5b3 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/PWM.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/PWM.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c  -o ${OBJECTDIR}/_ext/1324871677/PWM.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/PWM.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Robot.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c  .generated_files/flags/default/8b9d60d7501cdc0ed3e9b4be00bc039f88ade652 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Robot.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Robot.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c  -o ${OBJECTDIR}/_ext/1324871677/Robot.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Robot.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Timer.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c  .generated_files/flags/default/e4940bbe18f58ed363cd8600f6e0747d1aa5bcac .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Timer.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Timer.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c  -o ${OBJECTDIR}/_ext/1324871677/Timer.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Timer.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Toolbox.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c  .generated_files/flags/default/75e25248e6e22a42805b9674f708568a58cfc238 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Toolbox.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Toolbox.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c  -o ${OBJECTDIR}/_ext/1324871677/Toolbox.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Toolbox.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/main.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c  .generated_files/flags/default/adfe0562fdf636e2a656f8e9bd35d8cb24b01f49 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c  -o ${OBJECTDIR}/_ext/1324871677/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/main.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+else
+${OBJECTDIR}/_ext/1324871677/ADC.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c  .generated_files/flags/default/335ccc39db9fd5346f9e6883b12e740e4b025c04 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ADC.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ADC.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/ADC.c  -o ${OBJECTDIR}/_ext/1324871677/ADC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/ADC.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/ChipConfig.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c  .generated_files/flags/default/6c344541c63465628bdd9aed3735dae4d1eab43b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ChipConfig.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/ChipConfig.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/ChipConfig.c  -o ${OBJECTDIR}/_ext/1324871677/ChipConfig.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/ChipConfig.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/IO.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c  .generated_files/flags/default/39ad4f7fb59730a086cb42e21be68bca7ac0dee7 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/IO.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/IO.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/IO.c  -o ${OBJECTDIR}/_ext/1324871677/IO.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/IO.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/PWM.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c  .generated_files/flags/default/ce1be985bd4e56e15821b83e4097e591d798ed09 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/PWM.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/PWM.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/PWM.c  -o ${OBJECTDIR}/_ext/1324871677/PWM.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/PWM.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Robot.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c  .generated_files/flags/default/d2154f575392c72bb0f02e8a488e869f1ebe8858 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Robot.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Robot.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Robot.c  -o ${OBJECTDIR}/_ext/1324871677/Robot.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Robot.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Timer.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c  .generated_files/flags/default/9790a25243e8f8b2e97aecd4dc26800e621da7c4 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Timer.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Timer.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Timer.c  -o ${OBJECTDIR}/_ext/1324871677/Timer.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Timer.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/Toolbox.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c  .generated_files/flags/default/dc6270f8a3c39bd85fd7ae683c1e44c0de5022ed .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Toolbox.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/Toolbox.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/Toolbox.c  -o ${OBJECTDIR}/_ext/1324871677/Toolbox.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/Toolbox.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/_ext/1324871677/main.o: ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c  .generated_files/flags/default/b368806183d91a249fb7327e15c3a076364a54f1 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1324871677" 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1324871677/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../../../Users/E306-PC4/Desktop/sgsdgsg/main.c  -o ${OBJECTDIR}/_ext/1324871677/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1324871677/main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG   -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)      -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	
+else
+${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	${MP_CC_DIR}\\xc16-bin2hex ${DISTDIR}/ROBOT_Morlet_Lecat_3.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf   -mdfp="${DFP_DIR}/xc16" 
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(wildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
